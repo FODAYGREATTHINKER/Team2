7ce064144c2f293694033e4e8b390dbbd0e304cf
haridu <harindu96@gmail.com>
2016-10-24 14:27:15 +1300

    sprint 2

---
+++ medicalbookingapp/medicalbookingapp/src/com/example/medicalbookingapp/meddetails.java
@@ -0,0 +1,12 @@
+package com.example.medicalbookingapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class meddetails extends Activity{
+	
+	protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.medicaldetails);
+	}
+}
---
+++ medicalbookingapp/src/com/example/medicalbookingapp/MapsActivity.java
@@ -0,0 +1,20 @@
+package com.example.medicalbookingapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Page to view google maps activity. We could implement the API so this is
+ * empty.
+ *
+ */
+public class MapsActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.maps);
+	}
+
+}
