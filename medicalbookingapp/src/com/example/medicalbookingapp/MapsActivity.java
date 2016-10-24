c506aad0bf7f1df0460a3c8a503182209cd51ac3
haridu <harindu96@gmail.com>
2016-10-24 13:45:23 +1300

    final version

---
+++ medicalbookingapp/src/com/example/medicalbookingapp/MapsActivity.java
@@ -0,0 +1,21 @@
+/*package com.example.medicalbookingapp;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import android.os.Bundle;
+public class MapsActivity extends MapActivity {
+	  
+	    *//** Called when the activity is first created. *//*
+	    @Override
+	    public void onCreate(Bundle savedInstanceState)
+	    {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.maps);
+	    }
+	 
+	    @Override
+	    protected boolean isRouteDisplayed() {
+	        return false;
+	    }
+	   
+}
+*/
